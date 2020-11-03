af1412717101907dcb8fa4de1cd11f88f53f8596
brennamaloy <bkm91466@creighton.edu>
2020-11-03 12:49:45 -0600

    Create addBucketlistEvent.js

---
+++ forms/addBucketlistEvent/addBucketlistEvent.js
@@ -0,0 +1,12 @@
+req = ""
+query = ""
+results = ""
+pw = "JeremyBIA123"  // put your database password here
+userName = "jrp85607"
+database = "375groupa5"
+
+
+
+ddLocation.onclick=function(){
+  
+}
