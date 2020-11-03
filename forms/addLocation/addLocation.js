6ad51d4b1b573a9dbfcb602c28be0b744bf70116
brennamaloy <bkm91466@creighton.edu>
2020-11-03 12:21:17 -0600

    Update addLocation.js

--- forms/addLocation/addLocation.js
+++ forms/addLocation/addLocation.js
@@ -1,4 +1,11 @@
- //Geolocation - this sample gets the current location.
+req = ""
+query = ""
+results = ""
+pw = "bomber99"  // put your database password here
+userName = "bkm91466"
+database = "375groupa5"
+
+//Geolocation - this sample gets the current location.
  //It also shows the current speed and altitude.
 
  //Set up global variables
@@ -14,6 +21,8 @@ function Main() {
 btnSubmitLocation.onclick = function() {
  //This starts scanning the current location. It checks it every 5 seconds.
  //If it gets the data successfully, the handler function is called.
+ 
+ 
  //If not, errorCallBack is called.
   options={timeout: 5000, maximumAge: 5000, enableHighAccuracy: true};
   gps=navigator.geolocation.watchPosition(onGeolocation, errorCallBack, options);
@@ -34,10 +43,7 @@ function onGeolocation(location) {
  var s;
   s = "Longitude: " + location.coords.longitude  +  '\n';
   s = s  +  "Latitude: " + location.coords.latitude  +  '\n';
-  s = s  +  "Speed: " + location.coords.speed  +  " "  +  '\n';
-  s = s  +  "Altitude: " + location.coords.altitude  +  '\n';
-  s = s  +  "Accuracy: " + location.coords.accuracy  +  " "  +  '\n';
-  s = s  +  "Accuracy(altitude): " + location.coords.altitudeAccuracy  +  " "  +  '\n';
+ 
  //different browsers return the timestamp in different formats.
  //this converts it if necessary.
 
@@ -54,3 +60,31 @@ function errorCallBack(Error) {
  //unable to get the geolocation data
   NSB.MsgBox(Error.message);
 }
+
+let 
+
+btnAddLocation.onclick = function() {
+  query = "INSERT INTO locations VALUES ('Test')"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + userName + "&pass=" + pw + "&database=" + database + "&query=" + query)
+  if (req.status == 200) { //transit worked.
+    if (req.responseText == 500) { // means the insert succeeded
+      NSB.MsgBox("You have successfully added the Customer!")
+    } else
+     NSB.MsgBox("There was a problem with adding the Location to the database.")
+  } else {
+    // transit error
+    NSB.MsgBox("Error: " + req.status);
+  }
+
+  query = "SELECT longitude, latitude from locations"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + userName + "&pass=" + pw + "&database=" + database + "&query=" + query)
+
+  if (req.status == 200) { //transit worked.
+    //save the sate of the location
+    results = JSON.parse(req.responseText)
+  } else {
+    // transit error
+    NSB.MsgBox(`Error: ${req.status}`);
+  }
+  }
+  
