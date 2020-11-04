81b5393f8b773ebbaa5b54300930108eb75e7128
brennamaloy <bkm91466@creighton.edu>
2020-11-04 14:56:16 -0600

    Update addBucketlistEvent.js

--- forms/addBucketlistEvent/addBucketlistEvent.js
+++ forms/addBucketlistEvent/addBucketlistEvent.js
@@ -5,14 +5,11 @@ results = ""
 pw = "bomber99"  // put your database password here
 userName = "bkm91466"
 database = "375groupa5"
-<<<<<<< Updated upstream
 */
 addBucketlistEvent.onshow=function() {
   ddLocation.clear()
   query = "SELECT locations"
 }
-=======
->>>>>>> Stashed changes
 
 
 
