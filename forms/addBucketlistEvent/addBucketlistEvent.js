7457c6a029db34caccedceebafea144fce50df3a
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-11-03 20:59:07 -0600

    TeamUpdate

---
+++ forms/addBucketlistEvent/addBucketlistEvent.js
@@ -0,0 +1,18 @@
+/*
+req = ""
+query = ""
+results = ""
+pw = "bomber99"  // put your database password here
+userName = "bkm91466"
+database = "375groupa5"
+*/
+addBucketlistEvent.onshow=function() {
+  ddLocation.clear()
+  query = "SELECT locations"
+}
+
+
+
+btnCreateLocation.onclick=function(){
+  ChangeForm(addLocation)
+}
