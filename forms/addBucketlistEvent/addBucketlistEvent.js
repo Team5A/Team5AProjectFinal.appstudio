e7f9e8ab41146bba73f16e1e7967cab4e4b8e7ac
brennamaloy <bkm91466@creighton.edu>
2020-11-16 15:13:11 -0600

    Update addBucketlistEvent.js

--- forms/addBucketlistEvent/addBucketlistEvent.js
+++ forms/addBucketlistEvent/addBucketlistEvent.js
@@ -1,18 +1,26 @@
-/*
 req = ""
 query = ""
 results = ""
-pw = "bomber99"  // put your database password here
-userName = "bkm91466"
+pw = "JeremyBIA123"  // put your database password here
+userName = "jrp85607"
 database = "375groupa5"
-*/
-addBucketlistEvent.onshow=function() {
-  ddLocation.clear()
-  query = "SELECT locations"
+
+let bucketname = ""
+let bucketid = ""
+let bucketdate = ""
+let bucketdescription = ""
+
+btnAddEvent.onclick=function(){
+  bucketname = inptEventName.value
+  bucketid = inptUserID.value
+  bucketdate = inptCurrentDate.value
+  bucketdescription = inptBucketDescription.value
+
+  query = "INSERT INTO `events` (`bucket_id`, `event_name`, `date_completed`, `date_added`, `completed`, `location_id`, `journal_id`, `description`) VALUES (" + bucketid + ", '" + bucketname + "', NULL, '" + bucketdate + "', NULL, NULL, NULL, '" + bucketdescription +"');"
+  console.log(query)
+  
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + userName + "&pass=" + pw + "&database=" + database + "&query=" + query)
+
 }
 
 
-
-btnCreateLocation.onclick=function(){
-  ChangeForm(addLocation)
-}
